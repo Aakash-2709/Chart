f967689020602f649e21183a8f7a79f6975bb934
Aakash-2709 <109067511+Aakash-2709@users.noreply.github.com>
2023-06-14 11:42:03 +0530

    Update login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import ProductDetail from './ProductDetail';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import { Container } from 'react-bootstrap';
+// import ProductDetail from './ProductDetail';
 
 function App() {
-  
+
   return (
-    <ProductDetail />
+    <div className="wrapper">
+      <BrowserRouter>
+        <Container className="d-block">
+          <h1 className='text-center'>Order Chart</h1>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            {/* <Route path="/product" element={<ProductDetail />} /> */}
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </div>
   );
 }
 
