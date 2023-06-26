10029c7209964e2b694d397b4c298ea7d1cad14e
Aakash-2709 <109067511+Aakash-2709@users.noreply.github.com>
2023-06-26 09:58:48 +0530

    add dynamic data

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import CmsOreder from "./CmsOrder.json";
-import GdrxOrder from "./Gdrxorder.json";
+import React, { useEffect, useState } from 'react';
 
 const Test = () => {
-  
-  console.log(CmsOreder);
-  console.log(GdrxOrder);
-  let totalGdrxOr = GdrxOrder.reduce((total,value)=>{
+
+  const [gdrxOrder, setGdrx] = useState([])
+  const [CmsOreder, setCMS] = useState([])
+
+  const getGDRXApiData = async () => {
+    const response = await fetch(
+      "https://www.goodrxmedicine.com/customreport/index"
+    ).then((response) => response.json());
+
+    // update the state
+    setGdrx(response);
+  };
+  const getCMSApiData = async () => {
+    const response = await fetch(
+      "https://www.cheapmedicineshop.com/customreport/index"
+    ).then((response) => response.json());
+
+    // update the state
+    setCMS(response);
+  };
+  // getApiData()
+  useEffect(() => {
+    getCMSApiData();
+    getGDRXApiData();
+  }, []);
+
+
+  let totalGdrxOr = gdrxOrder.reduce((total, value) => {
     return total + +value.OrderCount
   }, 0)
-  let totalCmsOr = CmsOreder.reduce((total,value)=>{
+  let totalCmsOr = CmsOreder.reduce((total, value) => {
     return total + +value.OrderCount
   }, 0)
   const total = totalCmsOr + totalGdrxOr
@@ -20,15 +42,15 @@ const Test = () => {
         <h1>Order</h1>
         <div className="order">
           <span>CMS: {totalCmsOr}</span>
-          <span style={{margin:"0"}}>GDRX: {totalGdrxOr}</span>
+          <span style={{ margin: "0" }}>GDRX: {totalGdrxOr}</span>
         </div>
       </div>
-      <div className="total" style={{fontSize:"18px"}}>
+      <div className="total" style={{ fontSize: "18px" }}>
         <div className="color-div">
-        <div className="main-color">CMS <span className='cms'></span></div>
-        <div className="main-color">GDRX <span className='gdrx'></span></div>
+          <div className="main-color">CMS <span className='cms'></span></div>
+          <div className="main-color">GDRX <span className='gdrx'></span></div>
         </div>
-        <p style={{margin:"0"}}><strong>total Order : {total}</strong></p>
+        <p style={{ margin: "0" }}><strong>total Order : {total}</strong></p>
       </div>
       <div className="container">
         {
@@ -36,7 +58,7 @@ const Test = () => {
             <div className="box" key={index}>
               <p>{data.Orderdate}</p>
               <span className='gdrx'>{data.OrderCount}</span>
-              <span className='cms'>{GdrxOrder[index].OrderCount}</span>
+              <span className='cms'>{gdrxOrder[index]?.OrderCount}</span>
             </div>
           ))
         }
